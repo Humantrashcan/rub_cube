faf86316792220d171e3b7d1846506466afa0b37
Humantrashcan <humantrashcan@gmail.com>
2013-07-20 00:12:34 +0200

    print prev_cube
    commit before I define working and previous cubes needed to carry
    changes through the loop

--- solver.py
+++ solver.py
@@ -36,41 +36,49 @@ list_moves=[]
 def move_1():
     working_cube=[prev_cube[12],prev_cube[1],prev_cube[2],prev_cube[21],prev_cube[4],prev_cube[5],prev_cube[30],prev_cube[7],prev_cube[8],prev_cube[11],prev_cube[20],prev_cube[29],prev_cube[36],prev_cube[13],prev_cube[14],prev_cube[15],prev_cube[16],prev_cube[17],prev_cube[10],prev_cube[19],prev_cube[28],prev_cube[39],prev_cube[22],prev_cube[23],prev_cube[24],prev_cube[25],prev_cube[26],prev_cube[9],prev_cube[18],prev_cube[27],prev_cube[42],prev_cube[31],prev_cube[32],prev_cube[33],prev_cube[34],prev_cube[35],prev_cube[45],prev_cube[37],prev_cube[38],prev_cube[48],prev_cube[40],prev_cube[41],prev_cube[51],prev_cube[43],prev_cube[44],prev_cube[0],prev_cube[46],prev_cube[47],prev_cube[3],prev_cube[49],prev_cube[50],prev_cube[6],prev_cube[52],prev_cube[53]]
     print working_cube
+    print prev_cube
 
 # Move 2 - Left column moved down
 def move_2():
     working_cube=[prev_cube[45],prev_cube[1],prev_cube[2],prev_cube[48],prev_cube[4],prev_cube[5],prev_cube[51],prev_cube[7],prev_cube[8],prev_cube[27],prev_cube[18],prev_cube[9],prev_cube[0],prev_cube[13],prev_cube[14],prev_cube[15],prev_cube[16],prev_cube[17],prev_cube[28],prev_cube[19],prev_cube[10],prev_cube[3],prev_cube[22],prev_cube[23],prev_cube[24],prev_cube[25],prev_cube[26],prev_cube[29],prev_cube[20],prev_cube[11],prev_cube[6],prev_cube[31],prev_cube[32],prev_cube[33],prev_cube[34],prev_cube[35],prev_cube[12],prev_cube[37],prev_cube[38],prev_cube[21],prev_cube[40],prev_cube[41],prev_cube[30],prev_cube[43],prev_cube[44],prev_cube[36],prev_cube[46],prev_cube[47],prev_cube[39],prev_cube[49],prev_cube[50],prev_cube[42],prev_cube[52],prev_cube[53]]
     print working_cube
+    print prev_cube
 
 # Move 3 - Move right column up
 def move_3():
     working_cube=[prev_cube[0],prev_cube[1],prev_cube[14],prev_cube[3],prev_cube[4],prev_cube[23],prev_cube[6],prev_cube[7],prev_cube[32],prev_cube[9],prev_cube[10],prev_cube[11],prev_cube[12],prev_cube[13],prev_cube[38],prev_cube[33],prev_cube[24],prev_cube[15],prev_cube[18],prev_cube[19],prev_cube[20],prev_cube[21],prev_cube[22],prev_cube[41],prev_cube[34],prev_cube[25],prev_cube[16],prev_cube[27],prev_cube[28],prev_cube[29],prev_cube[30],prev_cube[31],prev_cube[44],prev_cube[35],prev_cube[26],prev_cube[17],prev_cube[36],prev_cube[37],prev_cube[47],prev_cube[39],prev_cube[40],prev_cube[50],prev_cube[42],prev_cube[43],prev_cube[53],prev_cube[45],prev_cube[46],prev_cube[2],prev_cube[48],prev_cube[49],prev_cube[5],prev_cube[51],prev_cube[52],prev_cube[8]]
     print working_cube
+    print prev_cube
 
 # Move 4 - Move right column down
 def move_4():
     working_cube=[prev_cube[0],prev_cube[1],prev_cube[47],prev_cube[3],prev_cube[4],prev_cube[50],prev_cube[6],prev_cube[7],prev_cube[53],prev_cube[9],prev_cube[10],prev_cube[11],prev_cube[12],prev_cube[13],prev_cube[2],prev_cube[17],prev_cube[26],prev_cube[35],prev_cube[18],prev_cube[19],prev_cube[20],prev_cube[21],prev_cube[22],prev_cube[5],prev_cube[16],prev_cube[25],prev_cube[34],prev_cube[27],prev_cube[28],prev_cube[29],prev_cube[30],prev_cube[31],prev_cube[8],prev_cube[15],prev_cube[24],prev_cube[33],prev_cube[36],prev_cube[37],prev_cube[14],prev_cube[39],prev_cube[40],prev_cube[23],prev_cube[42],prev_cube[43],prev_cube[32],prev_cube[45],prev_cube[46],prev_cube[38],prev_cube[48],prev_cube[49],prev_cube[41],prev_cube[51],prev_cube[52],prev_cube[44]]
     print working_cube
+    print prev_cube
     
 # Move 5 - Move top row left
 def move_5():
     working_cube=[prev_cube[6],prev_cube[3],prev_cube[0],prev_cube[7],prev_cube[4],prev_cube[1],prev_cube[8],prev_cube[5],prev_cube[2],prev_cube[12],prev_cube[13],prev_cube[14],prev_cube[15],prev_cube[16],prev_cube[17],prev_cube[53],prev_cube[52],prev_cube[51],prev_cube[18],prev_cube[19],prev_cube[20],prev_cube[21],prev_cube[22],prev_cube[23],prev_cube[24],prev_cube[25],prev_cube[26],prev_cube[27],prev_cube[28],prev_cube[29],prev_cube[30],prev_cube[31],prev_cube[32],prev_cube[33],prev_cube[34],prev_cube[35],prev_cube[36],prev_cube[37],prev_cube[38],prev_cube[39],prev_cube[40],prev_cube[41],prev_cube[42],prev_cube[43],prev_cube[44],prev_cube[45],prev_cube[46],prev_cube[47],prev_cube[48],prev_cube[49],prev_cube[50],prev_cube[9],prev_cube[10],prev_cube[11]]
     print working_cube
+    print prev_cube
 
 # Move 6 - Move top row left
 def move_6():
     working_cube=[prev_cube[2],prev_cube[5],prev_cube[8],prev_cube[1],prev_cube[4],prev_cube[7],prev_cube[0],prev_cube[3],prev_cube[6],prev_cube[53],prev_cube[52],prev_cube[51],prev_cube[9],prev_cube[10],prev_cube[11],prev_cube[12],prev_cube[13],prev_cube[14],prev_cube[18],prev_cube[19],prev_cube[20],prev_cube[21],prev_cube[22],prev_cube[23],prev_cube[24],prev_cube[25],prev_cube[26],prev_cube[27],prev_cube[28],prev_cube[29],prev_cube[30],prev_cube[31],prev_cube[32],prev_cube[33],prev_cube[34],prev_cube[35],prev_cube[36],prev_cube[37],prev_cube[38],prev_cube[39],prev_cube[40],prev_cube[41],prev_cube[42],prev_cube[43],prev_cube[44],prev_cube[45],prev_cube[46],prev_cube[47],prev_cube[48],prev_cube[49],prev_cube[50],prev_cube[17],prev_cube[16],prev_cube[15]]
     print working_cube
+    print prev_cube
 
 # Move 7 - Turn front face anti-clockwise
 def move_7():
     working_cube=[prev_cube[0],prev_cube[1],prev_cube[2],prev_cube[3],prev_cube[4],prev_cube[5],prev_cube[15],prev_cube[24],prev_cube[33],prev_cube[9],prev_cube[10],prev_cube[8],prev_cube[14],prev_cube[23],prev_cube[32],prev_cube[38],prev_cube[16],prev_cube[17],prev_cube[18],prev_cube[19],prev_cube[7],prev_cube[13],prev_cube[22],prev_cube[31],prev_cube[37],prev_cube[25],prev_cube[26],prev_cube[27],prev_cube[28],prev_cube[6],prev_cube[12],prev_cube[21],prev_cube[30],prev_cube[36],prev_cube[34],prev_cube[35],prev_cube[11],prev_cube[20],prev_cube[29],prev_cube[39],prev_cube[40],prev_cube[41],prev_cube[42],prev_cube[43],prev_cube[44],prev_cube[45],prev_cube[46],prev_cube[47],prev_cube[48],prev_cube[49],prev_cube[50],prev_cube[51],prev_cube[52],prev_cube[53]]
     print working_cube
+    print prev_cube
 
 # Move 8 - Turn front face clockwise
 def move_8():
     working_cube=[prev_cube[0],prev_cube[1],prev_cube[2],prev_cube[3],prev_cube[4],prev_cube[5],prev_cube[29],prev_cube[20],prev_cube[11],prev_cube[9],prev_cube[10],prev_cube[36],prev_cube[30],prev_cube[21],prev_cube[12],prev_cube[6],prev_cube[16],prev_cube[17],prev_cube[18],prev_cube[19],prev_cube[37],prev_cube[31],prev_cube[22],prev_cube[13],prev_cube[7],prev_cube[25],prev_cube[26],prev_cube[27],prev_cube[28],prev_cube[38],prev_cube[32],prev_cube[23],prev_cube[14],prev_cube[8],prev_cube[34],prev_cube[35],prev_cube[33],prev_cube[24],prev_cube[15],prev_cube[39],prev_cube[40],prev_cube[41],prev_cube[42],prev_cube[43],prev_cube[44],prev_cube[45],prev_cube[46],prev_cube[47],prev_cube[48],prev_cube[49],prev_cube[50],prev_cube[51],prev_cube[52],prev_cube[53]]
     print working_cube
+    print prev_cube
  
 
 
@@ -92,6 +100,8 @@ def rand_num():
     rand_number=0
     rand_number=random.randint(1,8)
     print rand_number
+    list_moves.append(rand_number)
+    print list_moves
     if rand_number==1:
         move_1()
     elif rand_number==2:
@@ -113,7 +123,9 @@ def rand_num():
 # Pick a random number, move the square, check whether it is OK, if yes stop, if no pick a random number - remember to keep a list
 def solver():
     rand_num()
+    prev_cube=list(working_cube)
     l1c0()
     print list_moves
 
+solver()
 solver()
