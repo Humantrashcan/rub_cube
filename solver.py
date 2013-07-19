a3d8352e98766f121ca17f8b90daec538e906466
Humantrashcan <humantrashcan@gmail.com>
2013-07-19 22:05:13 +0200

    5 moves completed

--- solver.py
+++ solver.py
@@ -17,8 +17,7 @@ Use Excel sheet
 
 """
 
-original_cube=["g","g","o","b","b","g","r","w","y","g","y","y","g","r","b","w","g","w","b","o","y","y","w","w","o","r","w","o","r","w","w","b","y","r","g","b","g","r","o","o","g","b","r","w","r","y","o","b","o","y","r","o","y","b",
-]
+original_cube=["g","g","o","b","b","g","r","w","y","y","y","w","g","r","b","r","o","w","y","o","r","y","w","w","g","r","g","g","b","o","w","b","y","b","w","w","g","r","o","o","g","b","r","w","r","y","o","b","o","y","r","o","y","b",]
 print original_cube
 
 working_cube=list(original_cube)     # blank cube will be used during the working 
@@ -36,10 +35,24 @@ def move_1():
     working_cube=[prev_cube[12],prev_cube[1],prev_cube[2],prev_cube[21],prev_cube[4],prev_cube[5],prev_cube[30],prev_cube[7],prev_cube[8],prev_cube[11],prev_cube[20],prev_cube[29],prev_cube[36],prev_cube[13],prev_cube[14],prev_cube[15],prev_cube[16],prev_cube[17],prev_cube[10],prev_cube[19],prev_cube[28],prev_cube[39],prev_cube[22],prev_cube[23],prev_cube[24],prev_cube[25],prev_cube[26],prev_cube[9],prev_cube[18],prev_cube[27],prev_cube[42],prev_cube[31],prev_cube[32],prev_cube[33],prev_cube[34],prev_cube[35],prev_cube[45],prev_cube[37],prev_cube[38],prev_cube[48],prev_cube[40],prev_cube[41],prev_cube[51],prev_cube[43],prev_cube[44],prev_cube[0],prev_cube[46],prev_cube[47],prev_cube[3],prev_cube[49],prev_cube[50],prev_cube[6],prev_cube[52],prev_cube[53]]
     print working_cube
 
+# Move 2 - Left column moved down
+def move_2():
+    working_cube=[prev_cube[45],prev_cube[1],prev_cube[2],prev_cube[48],prev_cube[4],prev_cube[5],prev_cube[51],prev_cube[7],prev_cube[8],prev_cube[27],prev_cube[18],prev_cube[9],prev_cube[0],prev_cube[13],prev_cube[14],prev_cube[15],prev_cube[16],prev_cube[17],prev_cube[28],prev_cube[19],prev_cube[10],prev_cube[3],prev_cube[22],prev_cube[23],prev_cube[24],prev_cube[25],prev_cube[26],prev_cube[29],prev_cube[20],prev_cube[11],prev_cube[6],prev_cube[31],prev_cube[32],prev_cube[33],prev_cube[34],prev_cube[35],prev_cube[12],prev_cube[37],prev_cube[38],prev_cube[21],prev_cube[40],prev_cube[41],prev_cube[30],prev_cube[43],prev_cube[44],prev_cube[36],prev_cube[46],prev_cube[47],prev_cube[39],prev_cube[49],prev_cube[50],prev_cube[42],prev_cube[52],prev_cube[53]]
+    print working_cube
 
+# Move 3 - Move right column up
+def move_3():
+    working_cube=[prev_cube[0],prev_cube[1],prev_cube[14],prev_cube[3],prev_cube[4],prev_cube[23],prev_cube[6],prev_cube[7],prev_cube[32],prev_cube[9],prev_cube[10],prev_cube[11],prev_cube[12],prev_cube[13],prev_cube[38],prev_cube[33],prev_cube[24],prev_cube[15],prev_cube[18],prev_cube[19],prev_cube[20],prev_cube[21],prev_cube[22],prev_cube[41],prev_cube[34],prev_cube[25],prev_cube[16],prev_cube[27],prev_cube[28],prev_cube[29],prev_cube[30],prev_cube[31],prev_cube[44],prev_cube[35],prev_cube[26],prev_cube[17],prev_cube[36],prev_cube[37],prev_cube[47],prev_cube[39],prev_cube[40],prev_cube[50],prev_cube[42],prev_cube[43],prev_cube[53],prev_cube[45],prev_cube[46],prev_cube[2],prev_cube[48],prev_cube[49],prev_cube[5],prev_cube[51],prev_cube[52],prev_cube[8]]
+    print working_cube
 
+# Move 4 - Move right column down
+def move_4():
+    working_cube=[prev_cube[0],prev_cube[1],prev_cube[47],prev_cube[3],prev_cube[4],prev_cube[50],prev_cube[6],prev_cube[7],prev_cube[53],prev_cube[9],prev_cube[10],prev_cube[11],prev_cube[12],prev_cube[13],prev_cube[2],prev_cube[17],prev_cube[26],prev_cube[35],prev_cube[18],prev_cube[19],prev_cube[20],prev_cube[21],prev_cube[22],prev_cube[5],prev_cube[16],prev_cube[25],prev_cube[34],prev_cube[27],prev_cube[28],prev_cube[29],prev_cube[30],prev_cube[31],prev_cube[8],prev_cube[15],prev_cube[24],prev_cube[33],prev_cube[36],prev_cube[37],prev_cube[14],prev_cube[39],prev_cube[40],prev_cube[23],prev_cube[42],prev_cube[43],prev_cube[32],prev_cube[45],prev_cube[46],prev_cube[38],prev_cube[48],prev_cube[49],prev_cube[41],prev_cube[51],prev_cube[52],prev_cube[44]]
+    print working_cube
+    
+# Move 5 - Move top row left
+def move_5():
+    working_cube=[prev_cube[6],prev_cube[3],prev_cube[0],prev_cube[7],prev_cube[4],prev_cube[1],prev_cube[8],prev_cube[5],prev_cube[2],prev_cube[12],prev_cube[13],prev_cube[14],prev_cube[15],prev_cube[16],prev_cube[17],prev_cube[53],prev_cube[52],prev_cube[51],prev_cube[18],prev_cube[19],prev_cube[20],prev_cube[21],prev_cube[22],prev_cube[23],prev_cube[24],prev_cube[25],prev_cube[26],prev_cube[27],prev_cube[28],prev_cube[29],prev_cube[30],prev_cube[31],prev_cube[32],prev_cube[33],prev_cube[34],prev_cube[35],prev_cube[36],prev_cube[37],prev_cube[38],prev_cube[39],prev_cube[40],prev_cube[41],prev_cube[42],prev_cube[43],prev_cube[44],prev_cube[45],prev_cube[46],prev_cube[47],prev_cube[48],prev_cube[49],prev_cube[50],prev_cube[9],prev_cube[10],prev_cube[11]]
+    print working_cube
 
-
-
-
-move_1()
+move_5()
